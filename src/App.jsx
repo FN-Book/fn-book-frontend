58e2c34dedbfdeded35c445ced4f0c1c9b09f6b7
rochamanuela <rochadesouzamanuela@gmail.com>
2024-10-15 21:05:38 -0300

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 import { About, AdminArea, Home } from './pages'
 import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
 
 export default function App() {
   return (
     <Routes>
-      {/* <Route element={<Navbar />}> */}
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/admin-area" element={<AdminArea />} />
         <Route path="/about" element={<About />} />
-      {/* </Route> */}
+      </Route>
     </Routes>
   )
 }
