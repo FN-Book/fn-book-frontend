9b8e577119e1b88460b35d2a03198452d8366ee2
rochamanuela <rochadesouzamanuela@gmail.com>
2024-11-01 21:24:26 -0300

    Continuação da implementação da tela home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import { Header, Carousel } from '../../components'
+import { Header, Carousel, Flag } from '../../components'
 import './style.css'
+
+import ImgMap from '../../assets/images/imagem-mapa.png'
 
 export default function Home() {
   return (
     <div className="main-container">
       <Header />
-      <div className="maps"></div>
-      {/* <div className="carousel"></div> */}
+      <div className="maps">
+        <div className="map-container">
+          <h1>Mapa de compartilhamento</h1>
+          <p>Veja onde se concentra a maior incidência de Fake News</p>
+          <img src={ImgMap} alt="" />
+        </div>
+        <div className="spotlight">
+          <p style={{marginBottom: '-30px'}}>Manchetes mais acessadas no momento</p>
+
+          <div className="spotlight-content">
+            <Flag truth={true} />
+            <h1 className="mini-card-title">#1 - EUA impõem sanções contra agentes ligados a operações russas</h1>
+            <p className="mini-card-text">Três entidades e 2 indivíduos serão sancionadas pelos EUA, segundo o secretário de Estado americano, Antony Blinken.</p>
+          </div>
+
+          <div className="spotlight-content">
+            <Flag truth={false} />
+            <h1 className="mini-card-title">#2 - EUA impõem sanções contra agentes ligados a operações russas</h1>
+            <p className="mini-card-text">Três entidades e 2 indivíduos serão sancionadas pelos EUA, segundo o secretário de Estado americano, Antony Blinken.</p>
+          </div>
+
+          <div className="spotlight-content">
+            <Flag truth={true} />
+            <h1 className="mini-card-title">#3 - EUA impõem sanções contra agentes ligados a operações russas</h1>
+            <p className="mini-card-text">Três entidades e 2 indivíduos serão sancionadas pelos EUA, segundo o secretário de Estado americano, Antony Blinken.</p>
+          </div>
+        </div>
+      </div>
       <Carousel />
       <hr />
       <div className="faq">
