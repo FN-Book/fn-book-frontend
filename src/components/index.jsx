4c989290173912bf1789f13e76608147c0d1aabb
rochamanuela <rochadesouzamanuela@gmail.com>
2024-11-08 13:29:55 -0300

    feat: create automatic slider

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,17 @@
+import AutomaticSlider from './AutomaticSlider'
 import Navbar from './Navbar'
 import Header from './Header'
 import Carousel from './Carousel'
 import MiniCard from './MiniCard'
 import Flag from './Flag'
+import DevInfo from './DevInfo'
 
 export {
+    AutomaticSlider,
     Navbar,
     Header,
     Carousel,
     MiniCard,
-    Flag
+    Flag,
+    DevInfo
 }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { AutomaticSlider, DevInfo } from '../../components'
+import imgJen from '../../assets/images/slider/photoUser.png'
+import imgJenColored from '../../assets/images/slider/photoUserColored.png'
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="container-about">
+      <AutomaticSlider>
+        <DevInfo
+          devName="Jennifer Costa"
+          devFunction="desenvolvedora frontend"
+          devPhoto={imgJen}
+          devPhotoHover={imgJenColored}
+        />
+        <DevInfo
+          devName="Manuela Souza"
+          devFunction="desenvolvedora frontend"
+          devPhoto={imgJen}
+          devPhotoHover={imgJenColored}
+        />
+      </AutomaticSlider>
+    </div>
   )
 }
